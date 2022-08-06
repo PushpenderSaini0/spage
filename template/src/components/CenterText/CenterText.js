080692f5ac3d14df85a375404261cf1b55c2026b
Pushpender Saini <54404738+PushpenderSaini0@users.noreply.github.com>
2022-08-06 13:24:49 +0530

    Add a template

---
+++ template/src/components/CenterText/CenterText.js
@@ -0,0 +1,12 @@
+import spage from "spagejs";
+
+export const CenterText = (prop) => {
+    const text = prop.text;
+    return (
+        <>
+            <h1 className="title">{text}</h1>
+        </>
+    );
+};
+
+
---
+++ template/src/components/HelloSpage.js
@@ -0,0 +1,8 @@
+import spage from "spagejs";
+import { CenterText } from "./CenterText/CenterText";
+
+const HelloSpage = () => {
+    return <CenterText text="Hello Spage" />;
+};
+
+export default HelloSpage;
